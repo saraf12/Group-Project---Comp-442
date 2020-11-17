810342bfe7443782dcda21e30c3091daa063e7a6
Lloyd <LLOYDTA18@GCC.EDU>
2020-11-16 20:25:07 -0500

    added matchup popup window

--- scripts/main.js
+++ scripts/main.js
@@ -2,10 +2,10 @@ window.addEventListener("DOMContentLoaded", function() {
     let requestBtn = document.getElementById("match-btn");
 	
     requestBtn.addEventListener("click", getMatch);
+
 });
 
 function getMatch(){
-    let pendingMatch = window.open("about:", "matchup", "width=00,height=200");
-    pendingMatch.document.write("Hello, world!");
+    popupWindow = window.open("../templates/matchup.html", "matchup", "width=500,height=500");
 }
 
