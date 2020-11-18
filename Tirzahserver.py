06850424c5ab3ecf2a5caebb965b0ab2506612d5
Lloyd <LLOYDTA18@GCC.EDU>
2020-11-17 20:29:46 -0500

    register page redirects to login and puts user info in db

--- Tirzahserver.py
+++ Tirzahserver.py
@@ -48,16 +48,78 @@ c.execute('''
 
 conn.commit()
 
-@app.route("/register/", methods=['POST'])
-def post_register():
-    #Get all data from the from
-    data = dict()
-    fields = ["username", "name","email", "password"]
-    for field in fields:
-        data[field] = request.form.get(field)
-    session["email"] = data["email"]
-    return redirect(url_for("get_register"))
+#save this key somewhere outside the database!
+key = Fernet.generate_key()
+pep = Fernet(key)
+
+#hash a password (pwd) and user pepper (pep)
+def hash_password(pwd, pep):
+    #compute the 14 round bcrypt hash
+    # of the sha256 hash of the password
+    h = bcrypt_sha256.using(rounds=14).hash(pwd)
+    # encrypt with AES (pepper)
+    ph = pep.encrypt(h.encode('utf-8'))
+    # encode as base64 string and return
+    b64ph = base64.b64encode(ph)
+    return b64ph
+
+#check password and pwd against b64ph
+def check_password(pwd, b64ph, pep):
+    # decode the encrypted base64 hash
+    ph = base64.b64decode(b64ph)
+    # decrypt the hash (remove pepper)
+    h = pep.decrypt(ph)
+    # let passlib check the hash
+    return bcrypt_sha256.verify(pwd, h)
+
+# @app.route("/register/", methods=['POST'])
+# def post_register():
+#     #Get all data from the from
+#     data = dict()
+#     fields = ["username", "name","email", "password"]
+#     for field in fields:
+#         data[field] = request.form.get(field)
+#     session["email"] = data["email"]
+#     return redirect(url_for("get_register"))
 
 @app.route("/register/", methods=["GET"])
 def get_register():
     return render_template("registerPage.html")
+
+@app.route("/signin/", methods=["GET"])
+def get_signin():
+    return render_template("signInPage.html")
+
+@app.route("/register/", methods=["POST"])
+def post_register():
+    regdb = get_db()
+    c = get_db().cursor()
+    data = dict()
+    fields = ['name', 'username', 'email', 'password', 'confirm-password']
+    for field in fields:
+        data[field] = request.form.get(field)
+    valid = True
+    for field in fields:
+        if data[field] is None or data[field] == "":
+            valid = False
+            flash(f"{field} cannot be blank")
+    if valid and len(data['password']) < 8:
+        valid = False
+        flash("password must be at least 8 characters")
+    if valid and data['password'] != data['confirm-password']:
+        valid = False
+        flash("password and confirm password must match")
+    if valid:
+        session['email'] = data['email']
+    uid = c.execute('SELECT id FROM Users WHERE email=?;',(data['email'],)).fetchone()
+    #if a user is found, then this email address is taken
+    if uid is not None:
+        flash("An account with this email already exists")
+        return redirect(url_for("get_register"))
+    # otherwise add them to the database and redirect to login
+    h = hash_password(data['password'], pep)
+    c.execute('INSERT INTO Users (email, passwordhash) VALUES (?,?);', (data['email'], h))
+    regdb.commit()
+    return redirect(url_for("get_signin"))
+
+    
