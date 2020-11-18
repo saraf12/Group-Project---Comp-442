fc72ed8a7336273136fb5442b7f94cc3b7029ab4
Sydnee S. Charles <sydnee12sarah@yahoo.fr>
2020-11-18 18:30:49 -0500

    Icon color change

---
+++ scripts/icon.js
@@ -0,0 +1,60 @@
+window.addEventListener("DOMContentLoaded", function() {
+    let list = document.getElementById("Ilist");
+
+    let iconsBtn = document.getElementsByClassName("icons");
+    var len = iconsBtn.length;
+    
+    
+    
+    for(var i = 0; i < len; i++){
+        iconsBtn[i].addEventListener("click", changeIcon);
+    }
+});
+var current = 0;
+
+function changeIcon(){
+    let profile = document.getElementById("Uicon");
+    var color = this.id;
+    switch (this.id){
+        case "black":
+            profile.src = "../static/userIcon/black.png";
+            break;
+        case "Blue":
+            profile.src = "../static/userIcon/Blue.png";
+            break;
+        case "darkBlue":
+            profile.src = "../static/userIcon/darkBlue.png";
+            break;
+        case "gray":
+            profile.src = "../static/userIcon/gray.png";
+            break;
+        case "green":
+            profile.src = "../static/userIcon/green.png";
+            break;
+        case "lightBlue":
+            profile.src = "../static/userIcon/lightBlue.png";
+            break;
+        case "lightGreen":
+            profile.src = "../static/userIcon/lightGreen.png";
+            break;
+        case "orange":
+            profile.src = "../static/userIcon/orange.png";
+            break;
+        case "pink":
+            profile.src = "../static/userIcon/pink.png";
+            break;
+        case "purple":
+            profile.src = "../static/userIcon/purple.png";
+            break;
+        case "red":
+            profile.src = "../static/userIcon/red.png";
+            break;
+        case "yellow":
+            profile.src = "../static/userIcon/yellow.png";
+            break;
+                    
+        default:
+            profile.src = "../static/userIcon/black.png";
+            break;
+    }
+}
