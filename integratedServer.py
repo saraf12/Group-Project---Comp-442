e5cdeff73cc2f50e747cb1fd7fe106aefde241f3
Lloyd <LLOYDTA18@GCC.EDU>
2020-12-02 13:56:12 -0500

    created a server to integrate code.  fixed the small bug on the edit profile page

--- integratedServer.py
+++ integratedServer.py
@@ -236,7 +236,6 @@ def get_main_page():
         flash("Please sign in")
         return redirect(url_for("get_signin"))
 
-
     # Code implementing a time frame until user is logged out
     try:
         exp = datetime.strptime(session.get("expires"), "%Y-%m-%dT%H:%M:%SZ")
@@ -254,7 +253,6 @@ def get_main_page():
     profileData['email'] = c.execute('SELECT email FROM Users WHERE id=?;', (curr_uid,)).fetchone()[0]
     profileData['icon'] = c.execute('SELECT icon FROM Users WHERE id=?;', (curr_uid,)).fetchone()[0]
 
-
     #Getting all the possible games
     gameOptions = dict()
     gamesCursor = get_db().cursor()
@@ -262,36 +260,14 @@ def get_main_page():
     for game in gamesinDB:
         gameOptions[game[0]] = game
     
-
-    ############## NOT WORKING ######################################
     # Getting leaderboard info for each game
     UserLists = dict()
     gmsCursor = get_db().cursor()
     allgms = gmsCursor.execute('SELECT id, name FROM Games;').fetchall()
     for gm in allgms:
-        print(gm)
         UserLists[gm[0]] = c.execute('''SELECT username, performanceRating FROM Stats JOIN Users ON Users.id=Stats.user WHERE game=? 
                    ORDER BY performanceRating DESC;''', (gm[0],)).fetchall()
-    
-    # print(UserLists)
-    # for key, user in UserLists.items():
-    #     print("this is the key: ")
-    #     print(key)
-    #     print("This is the user: ")
-    #     print(user)
-    #     print("trying to access inside of user...")
-    #     print(user[0][0])
 
-    # print(UserLists)
-
-    ############### NOT WORKING ######################################
-    #Trying to get just the leaderboard info for one game (Snake)
-    # snakeLeaders = []
-    # snakeLeaders = c.execute('''SELECT user, performanceRating FROM Stats WHERE game=1 
-    #                ORDER BY performanceRating DESC;''').fetchall()
-    # print(snakeLeaders)
-
-    # print(gameOptions)
     regdb.commit()
     return render_template("mainPage.html", profileData=profileData, UserList=UserLists, gameOptions=gameOptions)
 
@@ -314,58 +290,77 @@ def profile_page():
    #Get records
     regdb.row_factory = lambda cursor, row: row[0]
     c = regdb.cursor()
-    recordList = []
-    # NOTE: Need to change this get it from all games tables
-    matchesId = c.execute('SELECT id FROM Matches WHERE username1 =? OR username2=?',
-                (profileData['username'],profileData['username'],)).fetchall()
     
-    for mId in matchesId:
-        match = dict()
-        match["id"] = mId
-        match['c_username'] = profileData['username']
-        resp = ""
-        username1 = c.execute('SELECT username1 FROM Matches WHERE id = ?',(mId,)).fetchone()
-        username2 = c.execute('SELECT username2 FROM Matches WHERE id = ?', (mId,)).fetchone()
-        if(username1 == profileData['username']):
-            match['user'] = 1
-            match['username'] = username2
-            match['icon'] = c.execute('SELECT icon FROM Users WHERE username =?', (username2,)).fetchone()
-            resp = c.execute('SELECT response1 FROM Matches WHERE id=?',(mId,)).fetchone()
-        else:
-            match['user'] = 2
-            match['username'] = username1
-            match['icon'] = c.execute('SELECT icon FROM Users WHERE username =?', (username1,)).fetchone()
-            resp = c.execute('SELECT response2 FROM Matches WHERE id=?',(mId,)).fetchone()
+    # NOTE: Need to change this get it from all games tables
+    gamesRecords = dict()
+    gameStats = dict()
+    #For each game get Game name 
+    gamesName = c.execute('SELECT name FROM Games').fetchall()
 
-        if(resp == None):
-            match['response'] = 0
-        else:
-            match['response'] = 1
-        recordList.append(match)
+    for game in gamesName:
 
+        #for Get records from each match
+        matchesId = c.execute('SELECT id FROM {} WHERE username1 =? OR username2=?'.format(game),
+                    (profileData['username'],profileData['username'],)).fetchall()
+        recordList = []
+        for mId in matchesId:
+            match = dict()
+            #match["game"] = game
+            match["id"] = mId
+            match['c_username'] = profileData['username']
+            resp = ""
+            username1 = c.execute('SELECT username1 FROM {} WHERE id = ?'.format(game),(mId,)).fetchone()
+            username2 = c.execute('SELECT username2 FROM {} WHERE id = ?'.format(game), (mId,)).fetchone()
+            if(username1 == profileData['username']):
+                match['user'] = 1
+                match['username'] = username2
+                match['icon'] = c.execute('SELECT icon FROM Users WHERE username =?', (username2,)).fetchone()
+                resp = c.execute('SELECT winnerAccordingToU1 FROM {} WHERE id=?'.format(game),(mId,)).fetchone()
+            else:
+                match['user'] = 2
+                match['username'] = username1
+                match['icon'] = c.execute('SELECT icon FROM Users WHERE username =?', (username1,)).fetchone()
+                resp = c.execute('SELECT winnerAccordingToU2 FROM {} WHERE id=?'.format(game),(mId,)).fetchone()
 
-    return render_template("profilePage.html", profileData=profileData, records = recordList)
+            if(resp == None):
+                match['response'] = 0
+            else:
+                match['response'] = 1
+            recordList.append(match)
+        gamesRecords[game] = recordList
+        
+        #Get player state per game
+        stats = dict()
+        gameID = c.execute('SELECT id FROM Games WHERE name=?',(game,)).fetchone()
+        stats["performance"] = c.execute('SELECT performanceRating FROM Stats WHERE user=? AND game=?',(curr_uid,gameID,)).fetchone()
+        stats["wins"] = c.execute('SELECT wins FROM Stats WHERE user=? AND game=?',(curr_uid,gameID,)).fetchone()
+        stats["losses"] = c.execute('SELECT losses FROM Stats WHERE user=? AND game=?',(curr_uid,gameID,)).fetchone()
+        gameStats[game] = stats
+
+    return render_template("profile.html", profileData=profileData,gameStats=gameStats, gamesRecords = gamesRecords, gameslst = gamesName)
 
 
 
 @app.route("/profilepage/", methods=["POST"])
 def updaterecord():
+    game = request.form['game']
     matchId = request.form['matchId']
     name = "result" + str(matchId)
     r = request.form.getlist('result')
     user = int(request.form['user'])
     regdb = get_db()
     c = get_db().cursor()
-    c.execute('UPDATE Matches SET response1 =? WHERE id =?;',("Done", 1))
+    #c.execute('UPDATE {} SET winnerAccordingToU1 =? WHERE id =?;'.format(game),("Done", 1))
     if len(r) > 0:
         result = r[0]
         if(user == 1):
-            c.execute('UPDATE Matches SET response1 =? WHERE id =?;',(result, matchId))
+            c.execute('UPDATE {} SET winnerAccordingToU1 =? WHERE id =?;'.format(game),(result, matchId))
         else:
-            c.execute('UPDATE Matches SET response2 =? WHERE id =?;',(result, matchId))
+            c.execute('UPDATE {} SET winnerAccordingToU2 =? WHERE id =?;'.format(game),(result, matchId))
 
     regdb.commit()
     return redirect(url_for("profile_page"))
+
 
 
 @app.route("/editprofile/", methods=["GET"])
@@ -435,33 +430,24 @@ def get_matchup_window(gametype):
 
     currUserData['username'] = c.execute('SELECT username FROM Users WHERE id=?;', (curr_uid,)).fetchone()[0]
     currUserData['name'] = c.execute('SELECT name FROM Users WHERE id=?;', (curr_uid,)).fetchone()[0]
-    # NEED TO ADD CODE HERE GETTING THE USER'S PERFORMANCE RATING IN THIS GAME SO THAT THEY CAN BE MATCHED UP!!
     currUserData['email'] = c.execute('SELECT email FROM Users WHERE id=?;', (curr_uid,)).fetchone()[0]
     currUserData['icon'] = c.execute('SELECT icon FROM Users WHERE id=?;', (curr_uid,)).fetchone()[0]
 
-    # Code trying to get the current user's performance rating
+    # Code to get the current user's performance rating
     currUserData['performanceRating'] = c.execute('''SELECT performanceRating 
                                                     FROM Stats
                                                     WHERE user=? and game=?;''',
                                                     (curr_uid, gametype,)).fetchone()[0]
 
-    # DON'T WORK RIGHT NOW BECAUSE DON'T HAVE PERFORMANCE RATING SCHEMA SET YET
     lowerLimit = currUserData['performanceRating'] - 200
     upperLimit = currUserData['performanceRating'] + 200
 
     opponentData = c.execute('''SELECT username, email, performanceRating 
-                                                    FROM Stats JOIN Users ON Stats.user=Users.id
-                                                    WHERE performanceRating>=? AND performanceRating<=?
-                                                    AND NOT Stats.user=? 
-                                                    ORDER BY RANDOM() LIMIT 1;''',
-                                                    (lowerLimit, upperLimit, curr_uid,)).fetchone()
-
-    # opponentData = c.execute('''SELECT username, email, performanceRating FROM Users 
-    #    WHERE performanceRating >= ? AND performanceRating <= ? AND NOT id=? ORDER BY RANDOM() LIMIT 1;''', 
-    #     (lowerLimit, upperLimit, curr_uid,)).fetchone()
-
-    # opponentData = ("Billy", "b@email.com", 300)
-    
+                                FROM Stats JOIN Users ON Stats.user=Users.id
+                                WHERE performanceRating>=? AND performanceRating<=?
+                                AND NOT Stats.user=? 
+                                ORDER BY RANDOM() LIMIT 1;''',
+                                (lowerLimit, upperLimit, curr_uid,)).fetchone()
 
     return render_template("matchup.html", currUserData=currUserData, opponentData=opponentData, gametype=gametype)
 
@@ -476,26 +462,14 @@ def post_matchup_window_accept():
     currentUsername = request.form.get('current-username')
     opponentUsername = request.form.get('opponent-username')
 
-    # Code attempting to insert match data into appropriate table
+    # Code to insert match data into appropriate table
     gametype = request.form.get('gametype')
 
     gameTableName = c.execute('SELECT name FROM Games where id=?', (gametype,)).fetchone()[0]
 
-    print(gameTableName)
-
-
-    # sqlInsert = "INSERT INTO " + gameTableName + " (username1, username2, status) VALUES (?,?,?);"
-
-    # c.execute(sqlInsert,(currentUsername, opponentUsername, "Requested",))
-
     c.execute('INSERT INTO {} (username1, username2, status) VALUES (?,?,?);'.format(gameTableName),
                     (currentUsername, opponentUsername, "Requested",))
 
-
-
-    # # NEED TO BE UPDATED TO GET SENT TO THE CORRECT GAME TABLE
-    # c.execute('INSERT INTO Matches (username1, username2) VALUES (?,?);', 
-    #         (currentUsername, opponentUsername,))
     regdb.commit()
     return redirect(url_for("match_accepted"))
 
@@ -521,39 +495,24 @@ def get_inbox():
     c = get_db().cursor()
 
     currUsername = c.execute('SELECT username FROM Users WHERE id=?',(curr_uid,)).fetchone()[0]
-   # print(currUsername)
 
     requestedGames = dict()
     requesterOfGames = dict()
 
     allTypesOfGames = c.execute('SELECT id, name FROM Games;').fetchall()
 
-    #print(allTypesOfGames)
-
     for game in allTypesOfGames:
         gamesWithUserAsRequester = c.execute('SELECT id, username2, dateCreated, status FROM {} WHERE username1=?;'.format(game[1]),
                     (currUsername,)).fetchall()
-        # sqlSelect2 = "SELECT id, username2, dateCreated, status FROM " + game[1] + " WHERE username1=?;"
-        # gamesWithUserAsRequester = c.execute(sqlSelect2,(currUsername,)).fetchall()
         i = 0
         for gm in gamesWithUserAsRequester:
-            # print(gm)
             requesterOfGames[i] = (game[1], gm)
-            # print(requestedGames[i])
             i = i+1
         gamesWithUserAsResponder = c.execute('SELECT id, username1, dateCreated FROM {} WHERE username2=? AND status=\"Requested\";'.format(game[1]),
                     (currUsername,)).fetchall()
-        # sqlSelect1 = "SELECT id, username1, dateCreated FROM " + game[1] + " WHERE username2=? AND status=\"Requested\";"
-        # gamesWithUserAsResponder = c.execute(sqlSelect1,(currUsername,)).fetchall()
         for gm in gamesWithUserAsResponder:
-            # print(gm)
             requestedGames[i] = (game[1], gm)
-            # print(requestedGames[i])
-            i = i+1
-
-    # for request in requestedGames.values():
-    #     if request:
-    #         print(request[1][0])       
+            i = i+1   
 
     return render_template("inbox.html", requestedGames=requestedGames, requesterOfGames=requesterOfGames)
 
@@ -567,14 +526,10 @@ def post_inbox():
     regdb = get_db()
     c = get_db().cursor()
 
-
-
     if request.form.get('submit-btn') == "accept":
         gameId = request.form.get('gameid')
         gameTable = request.form.get('gametable')
         c.execute('UPDATE {} SET status=\"Confirmed\" WHERE id=?'.format(gameTable),(gameId,))
-        # sqlString = "UPDATE " + gameTable + " SET status=\"Confirmed\" WHERE id=" + gameId + ";"
-        # c.execute(sqlString)
     
     if request.form.get('submit-btn') == "decline":
         gameId = request.form.get('gameid')
@@ -582,8 +537,6 @@ def post_inbox():
         # ASK Sydney if she would rather a declined match be removed from the database
         # but it might be good to keep it in as declined, so that you can let the other person know it was declined
         c.execute('UPDATE {} SET status=\"Declined\" WHERE id=?'.format(gameTable),(gameId,))
-        # sqlString = "UPDATE " + gameTable + " SET status=\"Declined\" WHERE id=" + gameId + ";"
-        # c.execute(sqlString)
     
     regdb.commit()
 
