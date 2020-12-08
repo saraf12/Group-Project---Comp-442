6d0d2566e7ae217c9dfeb288b37f70960cf0abf3
Sydnee S. Charles <sydnee12sarah@yahoo.fr>
2020-12-08 01:43:30 -0500

    Sadmin

--- static/scripts/Sadmin.js
+++ static/scripts/Sadmin.js
@@ -1,5 +1,31 @@
 window.addEventListener("DOMContentLoaded", function() {
-    let g =  document.getElementById("sel");
-    console.log(g.value);
+    let tabBtn = document.getElementsByClassName("tab");
+    var len =tabBtn.length;
+    for(var i = 0; i < len; i++){
+        tabBtn[i].addEventListener("click", showTable);
+    }
+
+    let allBtn = document.getElementById("viewAll");
+    allBtn.addEventListener("click", showall);
     
 });
+
+function showTable(){
+    var tid = this.value;
+
+    let tables = document.getElementsByClassName("table");
+    var len = tables.length;
+    for(var i = 0; i < len; i++){
+        tables[i].style.display = "none";
+    }
+
+    document.getElementById(tid).style.display = "block";
+}
+
+function showall(){
+    let tables = document.getElementsByClassName("table");
+    var len = tables.length;
+    for(var i = 0; i < len; i++){
+        tables[i].style.display = "block";
+    }
+}
